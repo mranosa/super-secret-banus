368097eb29cb617d2ddb499df8687c1f936df195
mranosa <mark.ranosa@gmail.com>
2013-02-19 23:06:14 +0800

    Jsonifyed models

--- src/main/java/com/nci/Account.java
+++ src/main/java/com/nci/Account.java
@@ -3,11 +3,13 @@ package com.nci;
 import javax.validation.constraints.NotNull;
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.jpa.activerecord.RooJpaActiveRecord;
+import org.springframework.roo.addon.json.RooJson;
 import org.springframework.roo.addon.tostring.RooToString;
 
 @RooJavaBean
 @RooToString
 @RooJpaActiveRecord
+@RooJson
 public class Account {
 
     @NotNull
